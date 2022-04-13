8cbd00d7f6bc8fb5174a609f396f0ca15daf7ce7
Shankar Kumar <shankar0295@gmail.com>
2022-04-13 22:29:40 +0530

    maxnum,volume algorithm

--- work@tech/algorithms.js
+++ work@tech/algorithms.js
@@ -39,6 +39,12 @@ simpleIntrest(2532.4, 6.33, 3.67)
 
 const volumeofCyl = (r, h) => {
     const rup = (3.14 * (r * r) * h) * 40
-    return rup
+    return rup / 1000
 }
 volumeofCyl(5, 24)
+
+const Maxnum = (arr) => {
+    const decider = Math.max(...arr)
+    return (decider % 2 == 0) ? 'WON' : 'LOST'
+}
+Maxnum([12, 45, 234, 5674, 122])
